49bf6a649375fb272df127046ed49b86a4f3b62a
Rupendra Kumar Dhiman <Rdhiman@BHAVNACORP.NET>
2019-09-03 11:26:50 +0530

    Changes to check React life cycle method

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import { PrivateLogin } from '../PrivateLogin/PrivateLogin';
 import Login from '../Login/Login';
 import Navbar from '../Common/Navbar';
 
-function Home() {
-  return (<h1>Home</h1>);
+class Home extends React.Component {
+  state = {
+    pageTitle: ""
+  }
+
+  constructor(props) {
+    super(props);
+    console.log("Home - Constructor");
+  }
+
+  static getDerivedStateFromProps(p, s) {
+    console.log("Home - GetDrivedStateFromProps", p, s);
+    if (p.title !== s.pageTitle) {
+      return {
+        pageTitle: p.title
+      };
+    }
+    return null;
+  }
+
+  shouldComponentUpdate(p, s) {
+    console.log(p, s);
+    return true;
+  }
+
+  componentDidUpdate(pp, ps, s) {
+    console.log("Home - ComponentDidUpdate", pp, ps, s);
+  }
+
+  render() {
+    console.log("Home - Render");
+    return (
+      <div>
+        <h1>Home Page</h1>
+        <p>{this.state.pageTitle}</p>
+        <button onClick={this.props.titleChange}>Change Title</button>
+      </div>
+    );
+  }
 }
 
 function About() {
@@ -14,13 +51,23 @@ function About() {
 }
 
 function App() {
+  const t = "Home Page";
+
+  const [title, setTitle] = useState(t);
+
+  let countter = 0;
+  const titleChange = () => {
+    countter = countter++;
+    setTitle(t + "-" + countter);
+  }
+
   return (
     <Router>
       <div className="App">
         <Navbar />
         <div>
           <Route exact path="/login" component={Login}></Route>
-          <PrivateLogin exact path="/" component={Home}></PrivateLogin>
+          <PrivateLogin exact path="/" component={() => <Home title={title} titleChange={titleChange} />}></PrivateLogin>
           <PrivateLogin exact path="/about" component={About}></PrivateLogin>
         </div>
       </div>
